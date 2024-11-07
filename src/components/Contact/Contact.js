779b6c00e018c71fa6b020623cd44eaf307bc702
nisha <nishakulkarni2006@gmail.com>
2024-11-07 22:10:06 +0530

    updated resume

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,76 +1,7 @@
-// import React, { useRef, useState } from 'react';
-// import './Contact.css';
 import github from "../../photos/github.jpeg";
 import linkedin from "../../photos/Link.jpeg";
-// import emailjs from '@emailjs/browser';
 import { toast, Toaster } from 'react-hot-toast';
 import {motion} from "framer-motion";
-
-// const Contact = () => {
-//   const [data, setData] = useState(false);
-//   const form = useRef();
-
-//   const sendEmail = (e) => {
-//     e.preventDefault();
-
-//     emailjs
-//       .sendForm(
-//         'service_a7rb5ui',
-//         'template_dbaz61j',
-//         form.current,
-//         'VgynWsLOZaJcOA7gf'
-//       )
-//       .then(
-//         () => {
-//           console.log('SUCCESS!');
-//           setData(true);
-//           toast.success("Email sent successfully!");
-//         },
-//         (error) => {
-//           console.log('FAILED...', error.text);
-//           toast.error("Failed to send email. Please try again.");
-//         }
-//       );
-//   };
-
-//   return (
-//     <div className='Contact'>
-//       <Toaster /> {/* This line adds the Toaster component for displaying notifications */}
-//       <div
-//         className="blur"
-//         style={{ backgroundColor: 'rgb(300 220 260)', top: "160rem", left: "5rem" }}
-//       />
-//       <div className="social">
-//         <p> Get In Touch
-//           <br/>
-//           <span style={{color:"#fb982f"}}>Contact Me ! </span>
-//         </p>
-//         <div className="Sociallogos">
-//           <a href='https://www.linkedin.com/in/nisha-kulkarni-developer/'>
-//             <img src={linkedin} alt="LinkedIn" />
-//           </a>
-//           <a href='https://github.com/Nishakulkarni06'>
-//             <img src={github} alt="GitHub" />
-//           </a>
-//         </div>
-//       </div>
-//       <form ref={form} onSubmit={sendEmail}>
-//         <input placeholder='Name' name="user_name" required />
-//         <br/>
-//         <input placeholder='Email' name="user_email" type="email" required />
-//         <br/>
-//         <textarea placeholder='Message' name="message" required></textarea>
-//         <br/>
-//         <input type="submit" value="Submit" style={{ cursor: "pointer" }} />
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default Contact;
-
-
-
 import React, { useContext, useRef, useState } from "react";
 import "./Contact.css";
 import emailjs from "@emailjs/browser";
@@ -183,19 +114,11 @@ const Contact = () => {
             placeholder="Message"
             style={{ border: "2px solid #fb982f" }}
           />
-          {/* <input type="submit" value="Send" className="button" /> */}
-          {/* <button type="submit" className="button">Send</button> */}
-          <button
-  type="submit"
-  className="button"
-  onClick={() => console.log("Button clicked")}
->
-  Send
-</button>
+          <input type="submit" value="Send" className="button"/>
           <span>{done && "Thanks for Contacting me"}</span>
           <div
             className="blur c-blur1"
-            style={{ background: "var(--purple)" }}
+            style={{ background: "#fb982f" }}
           ></div>
         </form>
       </div>
